1deb457fae216afdfe58b7666fb059a69a8f72f5
SoraKy0 <154877077+SoraKy0@users.noreply.github.com>
2026-03-07 23:34:41 +0000

    Mission Number init

--- Remove duplicates/Remove duplicates (array).py
+++ Remove duplicates/Remove duplicates (array).py
@@ -1,7 +1,7 @@
 # Remove duplicate elements from an array
 
 def remove_duplicate():
-  array = [1,2,4,5,5,6,7,8,8,5,3,3,4,4,1,2,3,4,4,9,9,8]
+  array = [1,2,4,5,5,6,7,8,8,5,3,3,4,5,5,5,5,6,6,7,8,9,9,7,6,5,3,3,9,99,99,999,9999,99,999,9999,4,4,1,2,3,4,4,9,9,8]
   clean_array = []
  
   for num in array:
